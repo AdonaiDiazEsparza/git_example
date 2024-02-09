273dd10f625ac7e1923a5f23733686ee7e721413
Adonai Diaz <carlosadonai.de0208@gmail.com>
2024-02-09 16:45:31 -0600

    Commit V1.1

--- main.py
+++ main.py
@@ -4,13 +4,8 @@
 #       Im exiting for learn more about programming and this is one of 
 #       the things I need to know how it works, to share and work on team
 #
+#       NUM OF VERSION 1.1 
+#       so... here we go
+#
 #########################################################################
 
-print("This is version 1.0")
-
-#########################################################################
-#               SUMA DE UN NUMERO X Y UN NUMERO Y
-#########################################################################
-
-x,y = 10,7
-print(x+y)
