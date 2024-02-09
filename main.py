ea5e23911d263894737f73c13118dd62aed1282d
Adonai Diaz <carlosadonai.de0208@gmail.com>
2024-02-08 22:39:58 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#########################################################################
+#
+#       Hello this program is the first program I will use to prove Git 
+#       Im exiting for learn more about programming and this is one of 
+#       the things I need to know how it works, to share and work on team
+#
+#########################################################################
+
+print("This is version 1.0")
+
+#########################################################################
+#               SUMA DE UN NUMERO X Y UN NUMERO Y
+#########################################################################
+
+x,y = 10,7
+print(x+y)
