22db9f3ee4c712a4a5927ac771336cc288d6e188
Adonai Diaz <carlosadonai.de0208@gmail.com>
2024-02-09 19:31:49 -0600

    Final Commit

--- main.py
+++ main.py
@@ -4,8 +4,16 @@
 #       Im exiting for learn more about programming and this is one of 
 #       the things I need to know how it works, to share and work on team
 #
-#       NUM OF VERSION 1.1 
+#       NUM OF VERSION --I DONT HAVE FUCKIN IDEA
 #       so... here we go
 #
 #########################################################################
+
+def getForm(nameFile: str):
+    file = open(nameFile)
+    return file.read()
+
+text = getForm("heartForm.txt")
+
+print(text)
 
